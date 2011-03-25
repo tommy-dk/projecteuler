13badd325c21cb384311f90fcd0ef51e53f26675
Tommy Heyde Olsen <tho@danskespil.dk>
2011-03-25 15:02:17 +0100

    Changed p37 to use euler module

--- p37.py
+++ p37.py
@@ -8,40 +8,11 @@ NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 
 """
 
+from euler import primes, is_prime
+
 import time
 st = time.time()
 
-def primes(n): 
-    if n==2: return [2]
-    elif n<2: return []
-    s=range(3,n+1,2)
-    mroot = n ** 0.5
-    half=(n+1)/2-1
-    i=0
-    m=3
-    while m <= mroot:
-        if s[i]:
-            j=(m*m-3)/2
-            s[j]=0
-            while j<half:
-                s[j]=0
-                j+=m
-        i=i+1
-        m=2*i+3
-    return [2]+[x for x in s if x]
-
-def is_prime(n):
-    if n < 2:
-        return False
-    import math
-    n = int(n)
-    n = abs(n)
-    i = 2
-    while i <= math.sqrt(n):
-        if n % i == 0:
-            return False
-        i += 1
-    return True
 
 def truncate_left(n):
     for i in xrange(1,len(str(n))):
